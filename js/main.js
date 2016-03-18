afbd80222d7e373c57ffeb598d36f07c3f2b99a0
erinmilling <mill0814@algonquinlive.com>
2016-03-18 10:06:56 -0400

    adjusted css

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-var $ball = $('.ball');
-var $position = Math.random();
 var $body = $('body');
 
-$('html').on('key-down', function (e) {
-  var name = $('div');
+$('html').on('keydown', function (e) {
+  var $name = $('<div>');
   var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
   var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
 
   $name.addClass('ball');
-  $body.append('$name');
+  $body.append($name);
   $name.css('left', newLeft);
   $name.css('top', newTop);
 });
 
-document.write($random)
