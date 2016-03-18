568a60e85349cd1c612bc4455f5c57e801a2febe
erinmilling <mill0814@algonquinlive.com>
2016-03-18 10:02:40 -0400

    tried to make work

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var $ball = $('.ball');
 var $position = Math.random();
+var $body = $('body');
 
 $('html').on('key-down', function (e) {
-  var ballDown = $ball.down().top - 10;
-  $ball.css('top', ballDown);
+  var name = $('div');
+  var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
+  var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
+
+  $name.addClass('ball');
+  $body.append('$name');
+  $name.css('left', newLeft);
+  $name.css('top', newTop);
 });
 
 document.write($random)
